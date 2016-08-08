0a78265c6b321df4cbb5f4880c103ce9488397e0
XuDeHong <740665370@qq.com>
2016-08-08 21:39:11 +0800

    The First Day

---
+++ CostList/CostList/ChartTableViewController.m
@@ -0,0 +1,81 @@
+//
+//  ChartTableViewController.m
+//  CostList
+//
+//  Created by 许德鸿 on 16/8/8.
+//  Copyright © 2016年 XuDeHong. All rights reserved.
+//
+
+#import "ChartTableViewController.h"
+
+@interface ChartTableViewController ()
+
+@property (weak, nonatomic) IBOutlet UINavigationBar *navigationBar;
+
+@end
+
+@implementation ChartTableViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    [self customizeAppearence]; //设置UI元素
+}
+
+-(void)customizeAppearence
+{
+    //设置TabBar的tintColor
+    self.tabBarController.tabBar.tintColor = self.view.tintColor;
+    
+    //设置NavigationBar完全透明
+    [self.navigationBar setBackgroundImage:[UIImage imageNamed:@"NavigationBarColor"] forBarMetrics:UIBarMetricsCompact];
+    
+    //设置NavigationBarItem的颜色
+    self.navigationBar.tintColor = [UIColor whiteColor];
+    
+    //去除NavigationBar的横线
+    if ([self.navigationBar respondsToSelector:@selector(setBackgroundImage:forBarMetrics:)]){
+        NSArray *list=self.navigationBar.subviews;
+        for (id obj in list) {
+            if ([obj isKindOfClass:[UIImageView class]]) {
+                UIImageView *imageView=(UIImageView *)obj;
+                NSArray *list2=imageView.subviews;
+                for (id obj2 in list2) {
+                    if ([obj2 isKindOfClass:[UIImageView class]]) {
+                        UIImageView *imageView2=(UIImageView *)obj2;
+                        imageView2.hidden=YES;
+                    }
+                }
+            }
+        }
+    }
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return 0;
+}
+
+/*
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:<#@"reuseIdentifier"#> forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+*/
+
+
+
+@end
